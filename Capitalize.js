fb5982f85b24e185e3ccc880c512877e93e47fb4
arik509 <sabirhossainarik34@gmail.com>
2026-01-08 13:37:23 +0600

    Solved Capitalize the first letter problem

---
+++ Capitalize.js
@@ -0,0 +1,14 @@
+function capitalizeWords(str) {
+    const words = str.split(' ');
+    
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    
+    return words.join(' ');
+  }
+  
+  console.log(capitalizeWords("hello world")); 
+  console.log(capitalizeWords("programming hero scic")); 
+  
+  
