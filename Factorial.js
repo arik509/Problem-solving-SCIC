754fd42cbe80bbeac2ded0013c0fa37a50396a4b
arik509 <sabirhossainarik34@gmail.com>
2026-01-08 13:39:20 +0600

    Solved Factorial Problem

---
+++ Factorial.js
@@ -0,0 +1,14 @@
+function factorial(n) {
+    let result = 1;
+    
+    for (let i = 1; i <= n; i++) {
+      result *= i;
+    }
+    
+    return result;
+  }
+  
+  console.log(factorial(5));
+  console.log(factorial(7));
+  
+  
